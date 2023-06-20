90a32789a6ae416628f2b8b62bd68a8979c55cd5
mboufern <boufernana17@gmail.com>
2023-06-20 12:02:01 +0000

    upadating the random_state for spliting

--- dataProcessingV2.py
+++ dataProcessingV2.py
@@ -69,7 +69,7 @@ labels = np.array([d[-1] for d in data])
 
 print(coordinates)
 
-X_train, X_test, y_train, y_test = train_test_split(coordinates, labels, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(coordinates, labels, test_size=0.2, random_state=1)
 
 scaler = MinMaxScaler()
 X_train_scaled = scaler.fit_transform(X_train)
